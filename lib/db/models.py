487758bc36bb99667d05e81555e5d71203e4bb39
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-09-08 23:33:46 -0400

    update models.py

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from datetime import datetime
 from sqlalchemy.orm import relationship, backref
-from sqlalchemy.orm import sessionmaker
-
 
 engine = create_engine('sqlite:///banking.db')
 
@@ -12,13 +11,19 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__='user'
+    __tablename__ = 'user'
 
-    account_no = Column(Integer(), primary_key=True)
+    id = Column(Integer(), primary_key=True)
     name = Column(String())
     account_type = Column(String())
-    balance = Column(Integer())
-    branch_code =Column(String())
+    account_balance=Column(Integer())
+    branch_id=Column(Integer())
 
 
-
+class Branch(Base):
+    __tablename__ = 'branch'
+    id = Column(Integer(), primary_key=True)
+    branch_address= Column(String())
+    branch_contactno=Column(String())
+    
+    
